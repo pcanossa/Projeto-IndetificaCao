810381ba461f72d2ab0c71f53a4fbb1ea82b57e8
Patrícia Canossa <p.canossa@hotmail.com>
2023-04-18 14:31:10 -0300

    Modificado ícone do navbar user e iniciado as validações de cadastro do usuário (inserido box de erros de validação)

--- src/app/pages/home/cadastro-user/cadastro-user.component.ts
+++ src/app/pages/home/cadastro-user/cadastro-user.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroUserComponent implements OnInit {
 
+  validError: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  verificaError():void {
+    this.validError = true;
   }
 
 }
